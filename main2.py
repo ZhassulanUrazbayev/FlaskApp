b3b11ba0911d088aa45d2ca714056197b23ff9ff
Zhassulan Urazbayev <160107058@stu.sdu.edu.kz>
2020-11-24 09:38:00 +0600

    first commit

---
+++ main2.py
@@ -0,0 +1,26 @@
+from flask import Flask, redirect
+
+app = Flask(__name__)
+
+number = 0
+
+chatInviteLinkIndex = 0
+
+chatInviteLinks = ["https://yandex.kz/", "https://www.google.com/"]
+
+@app.route('/')
+def index():
+    global number
+    number +=1
+    global chatInviteLinkIndex
+    if number >= 256:
+        if chatInviteLinkIndex == len(chatInviteLinks)-1:
+            chatInviteLinkIndex = chatInviteLinkIndex
+        else:
+            chatInviteLinkIndex += 1
+        return redirect(chatInviteLinks[chatInviteLinkIndex])
+    else:
+        return redirect(chatInviteLinks[chatInviteLinkIndex])
+
+if __name__ == "__main__":
+    app.run(debug=True)
